400937a9ed623320d003bdfd82fa0d2cd2c850b9
ismael <ismael@infoway-pi.com.br>
2016-04-08 16:36:16 -0300

    conflict

--- jhbu-webapp/src/main/java/br/com/infowaypi/jheat/usage/servlet/AuthRedirectServlet.java
+++ jhbu-webapp/src/main/java/br/com/infowaypi/jheat/usage/servlet/AuthRedirectServlet.java
@@ -16,8 +16,7 @@ public class AuthRedirectServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String id = req.getParameter("report_engine_id");
-		ReportManager.getInstance().registerReportEngine(reportEngineId, reportEngine);
-		super.doGet(req, resp);
+//		ReportManager.getInstance().registerReportEngine(reportEngineId, reportEngine);
 	}
 
 }
